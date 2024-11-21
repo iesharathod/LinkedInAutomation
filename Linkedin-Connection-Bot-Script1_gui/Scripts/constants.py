4b6ff600986f251ad907013895f042107c410e66
Isha Rathod <94354005+iesharathod@users.noreply.github.com>
2024-11-21 14:30:49 +0530

    Add files via upload

---
+++ Linkedin-Connection-Bot-Script1_gui/Scripts/constants.py
@@ -0,0 +1,29 @@
+# ;==========================================
+# ; Title:  Selenium Connection Bot
+# ; Authors: Prabal Jain and Chandan Gupta
+# ; Year: 2021
+# ; rel: Beta W GUI
+# ;==========================================
+
+
+connection_mess = ''
+# upto pages is just to let the script know for how many pages you want it to execute
+upto_page = 1
+
+username = 'your_linkedin_username'
+password = 'your_linkedin_password'
+commaseparated = 'keyword1;keyword2;keyword3'
+upto_page = '3'
+
+class Bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Just to add some colors in text
